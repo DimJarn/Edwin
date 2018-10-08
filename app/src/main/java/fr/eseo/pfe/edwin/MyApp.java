4d5dcc3373a2f65a8bb951de2e4ccdec7fba877a
Dimitri Jarneau <dimitri.jarneau@reseau.eseo.fr>
2018-10-09 00:24:40 +0200

    Test page de chargement (ecran bleu de 4s avant Hello world)

---
+++ app/src/main/java/fr/eseo/pfe/edwin/MyApp.java
@@ -0,0 +1,12 @@
+package fr.eseo.pfe.edwin;
+
+import android.app.Application;
+import android.os.SystemClock;
+
+public class MyApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        SystemClock.sleep(4000); //temps d'attente de 4s
+    }
+}
