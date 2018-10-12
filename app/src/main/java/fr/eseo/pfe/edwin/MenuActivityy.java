82adbc177cc21b6e4159e509afbe8f37caa3f127
julesjaunay <julesjaunay@hotmail.fr>
2018-10-12 10:23:26 +0200

    - Correction du commit et test

--- app/src/main/java/fr/eseo/pfe/edwin/MenuActivityy.java
+++ app/src/main/java/fr/eseo/pfe/edwin/MenuActivityy.java
@@ -31,6 +31,8 @@ public class MenuActivityy extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        //navigationView.setItemIconTintList(null);
     }
 
     @Override
