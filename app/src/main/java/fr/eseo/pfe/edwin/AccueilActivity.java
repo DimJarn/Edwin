c3e8ab6b1eb078c7ae532bc8666e1bdfe0f6e73a
Dimitri Jarneau <dimitri.jarneau@reseau.eseo.fr>
2018-11-01 18:59:45 +0100

    FActivité accueil (manque redirection vers d'autres frag)

--- app/src/main/java/fr/eseo/pfe/edwin/AccueilActivity.java
+++ app/src/main/java/fr/eseo/pfe/edwin/AccueilActivity.java
@@ -3,19 +3,49 @@ package fr.eseo.pfe.edwin;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-public class AccueilActivity extends Fragment {
+public class AccueilActivity extends Fragment implements View.OnClickListener {
 
     public static AccueilActivity newInstance() {
         return (new AccueilActivity());
     }
 
+    Fragment AideActivity;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.accueil, container, false);
+        View view = inflater.inflate(R.layout.accueil, container, false);
+
+        Button buttonHelp = (Button) view.findViewById(R.id.buttonNeedHelp);
+        buttonHelp.setOnClickListener(this);
+
+        Button buttonYourOperation = (Button) view.findViewById(R.id.buttonYourOperation);
+        buttonYourOperation.setOnClickListener(this);
+
+        Button buttonMoreInfo = (Button) view.findViewById(R.id.buttonMoreInfo);
+        buttonMoreInfo.setOnClickListener(this);
+
+        return view;
+    }
+
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.buttonYourOperation:
+                //Start activity one
+                System.out.println("BUGGgG");
+                break;
+            case R.id.buttonNeedHelp:
+                System.out.println("TEST");
+                Fragment AideActivity = new AideActivity();
+                break;
+            case R.id.buttonMoreInfo:
+                System.out.println("XXVVVVVMMMM");
+                break;
+            // Do this for all buttons.
+        }
     }
 }
