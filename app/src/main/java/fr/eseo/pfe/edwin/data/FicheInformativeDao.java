581bc5de0af5b0667456ff0250ff24eaeda6f3bc
Dimitri Jarneau <dimitri.jarneau@reseau.eseo.fr>
2018-11-21 08:41:21 +0100

    ficheInfo Dao

---
+++ app/src/main/java/fr/eseo/pfe/edwin/data/FicheInformativeDao.java
@@ -0,0 +1,35 @@
+package fr.eseo.pfe.edwin.data;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
+
+import java.util.List;
+
+@Dao
+public interface FicheInformativeDao {
+
+    @Insert
+    public void insertFicheInformative(FicheInformative ficheInformative);
+
+    @Update
+    public void updateFicheInformative(FicheInformative ficheInformative);
+
+    @Delete
+    public void deleteFicheInformative(FicheInformative ficheInformative);
+
+    @Query("SELECT * FROM fiche_informative")
+    public List<FicheInformative> findAllFichesInformatives();
+
+    @Query("SELECT * FROM fiche_informative WHERE id_fiche = :idFiche")
+    public FicheInformative findFicheInformativeFromId(int idFiche);
+
+    @Query("SELECT * FROM contenu_fiche WHERE id_contenu_fiche = :idFiche")
+    public FicheInformative findFicheInformativeFromIdAndContent(int idFiche);
+
+
+
+
+}
